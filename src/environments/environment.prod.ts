8f3acbbf247eac3662b96ee938efe764fc363ebb
Basil K Baby <basil@mailcohen.com>
2026-05-28 13:30:53 +0100

    commit changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://crowdpass.azurewebsites.net/api',
+  apiUrl: 'https://v4entertainment.azurewebsites.net/api',
   appName: 'Identity Service',
   identityUrl: 'https://your-identity-service.com',
   identityAPIUrl: ' http://localhost:55884'
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://crowdpass.azurewebsites.net/api',
+  apiUrl: 'https://v4entertainment.azurewebsites.net/api',
   appName: 'Identity Service',
   identityUrl: 'http://localhost:55884', // Your identity service
   identityAPIUrl: 'https://localhost:7114' // Your identity service
