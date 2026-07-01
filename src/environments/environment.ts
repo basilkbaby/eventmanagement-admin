407507cc306746904497de10129f6ae7a0cc707f
Basil K Baby <basil@mailcohen.com>
2026-07-01 14:05:51 +0100

    commit chnages

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://localhost:7144/api',
+  apiUrl: 'https://v4entertainment.azurewebsites.net/api',
   appName: 'Identity Service',
   identityUrl: 'http://localhost:55884', // Your identity service
   identityAPIUrl: 'https://localhost:7114' // Your identity service
